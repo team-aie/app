d3c41152db7463440ea9745c20d752615408758e
Iluminar <yi.iluminar@gmail.com>
2020-05-01 04:15:05 -0700

    chore: Remove labels from release assets because they are not helpful and cannot be edited on the web page

--- release.config.js
+++ release.config.js
@@ -20,12 +20,12 @@ module.exports = {
             '@semantic-release/github',
             {
               assets: [
-                { path: 'dist/*.zip', label: 'Packaged Zip File' },
-                { path: 'dist/*.dmg', label: 'Packaged macOS App' },
-                { path: 'dist/*.exe', label: 'Packaged Windows App' },
-                { path: 'dist/*.AppImage', label: 'Packaged Linux App' },
-                { path: 'dist/*.yml', label: 'AutoUpdate Metadata' },
-                { path: 'dist/*.blockmap', label: 'AutoUpdate Blockmap' },
+                { path: 'dist/*.zip' },
+                { path: 'dist/*.dmg' },
+                { path: 'dist/*.exe' },
+                { path: 'dist/*.AppImage' },
+                { path: 'dist/*.yml' },
+                { path: 'dist/*.blockmap' },
               ],
             },
           ],
