09858a176dbc282e269bdcacc67d5e306ed9ade2
Iluminar <yi.iluminar@gmail.com>
2020-05-15 15:33:11 -0700

    chore: Remove need for semantic-release in check version step

--- release.config.js
+++ release.config.js
@@ -1,34 +1,28 @@
 'use strict';
 
-const calculateVersionOnly = process.env.CALCULATE_VERSION_ONLY === 'true';
-
 module.exports = {
   branches: [
     {
       name: 'master',
     },
   ],
-  ...(calculateVersionOnly
-    ? { plugins: ['@semantic-release/commit-analyzer'], dryRun: true }
-    : {
-        plugins: [
-          '@semantic-release/commit-analyzer',
-          '@semantic-release/release-notes-generator',
-          ['@semantic-release/npm', { npmPublish: false }],
-          '@semantic-release/git',
-          [
-            '@semantic-release/github',
-            {
-              assets: [
-                { path: 'dist/*.zip' },
-                { path: 'dist/*.dmg' },
-                { path: 'dist/*.exe' },
-                { path: 'dist/*.AppImage' },
-                { path: 'dist/*.yml' },
-                { path: 'dist/*.blockmap' },
-              ],
-            },
-          ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', { npmPublish: false }],
+    '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'dist/*.zip' },
+          { path: 'dist/*.dmg' },
+          { path: 'dist/*.exe' },
+          { path: 'dist/*.AppImage' },
+          { path: 'dist/*.yml' },
+          { path: 'dist/*.blockmap' },
         ],
-      }),
+      },
+    ],
+  ],
 };
