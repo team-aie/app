620976233fc04bc1cb8a85b2dd7380dc60e2b489
Iluminar <yi.iluminar@gmail.com>
2020-07-08 21:35:08 -0700

    feat: Add built-in recording list "Z Chinese CVVC - Normal"

--- src/renderer/components/configure-recording-set-page/types.ts
+++ src/renderer/components/configure-recording-set-page/types.ts
@@ -1,1 +1,3 @@
-export type BuiltInRecordingList = 'デルタ式英語リストver5 (Delta English Ver. 5)';
+export type BuiltInRecordingList =
+  | 'デルタ式英語リストver5 (Delta English Ver. 5)'
+  | 'Z式CVVC-Normal (Z Chinese CVVC - Normal)';
