0553109a55d646692afc953dd4e01640a394b244
garretje <garretje@rose-hulman.edu>
2020-10-25 14:51:57 -0400

    added selecting with space bar

--- src/renderer/components/back-button/index.tsx
+++ src/renderer/components/back-button/index.tsx
@@ -1,28 +1,17 @@
 import React, { FC, MouseEventHandler } from 'react';
+import { Button } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
 import { Positional } from '../helper-components';
 
 import backButton from './back-button.svg';
 
-// const buttonStyle = {
-//   width: '1rem',
-// };
-
-// const highlightButtonStyle = {
-//   borderStyle: 'solid',
-//   borderColor: '#cb4335',
-// }
-
-// const selectedButtonStyle = {
-//   borderStyle: 'solid',
-//   borderColor: 'blue',
-// }
-
 const BackButton: FC<{ onBack?: MouseEventHandler<HTMLElement> }> = ({ onBack }) => {
   return (
     <Positional position={'top-left'}>
-      <Image src={backButton} style={{ width: '1rem' }} onClick={onBack} className="highlightButton" tabIndex={0} />
+      <Button onClick={onBack} variant={'outline-secondary'} className="highlightButton">
+        <Image src={backButton} style={{ width: '1rem' }} />
+      </Button>
     </Positional>
   );
 };
