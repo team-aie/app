e214e6c32c1b3334cd90af1982ef9f5cb36f3942
robinlingyunli <lil4@rose-hulman.edu>
2020-11-04 00:06:09 -0500

    put switch button under welcome page to keep it consistent

--- src/renderer/components/style-switcher/index.tsx
+++ src/renderer/components/style-switcher/index.tsx
@@ -1,10 +1,6 @@
 import React, { FC, useContext } from 'react';
-import Image from 'react-bootstrap/Image';
 
 import { LocaleContext } from '../../contexts';
-import { SupportedLocale } from '../../types';
-
-import switchButton from './styleSwitchButton.svg';
 
 const StyleSwitcher: FC = () => {
   const { locale } = useContext(LocaleContext);
@@ -15,26 +11,5 @@ const StyleSwitcher: FC = () => {
     </div>
   );
 };
-const StyleButton: FC = () => {
-  const { locale, setLocale } = useContext(LocaleContext);
-  const styleHref = `${locale}.styles.css`;
-  return (
-    <div>
-      <Image
-        onClick={(): void => {
-          if (locale.substring(locale.length - 4) == 'dark') {
-            setLocale(locale.substring(0, locale.length - 5) as SupportedLocale);
-          } else {
-            setLocale(`${locale}-dark` as SupportedLocale);
-          }
-        }}
-        src={switchButton}
-        style={{ width: '2rem' }}
-      />
-      <link href={styleHref} rel={'stylesheet'} />
-    </div>
-  );
-};
 
-export { StyleSwitcher };
-export { StyleButton };
+export default StyleSwitcher;
