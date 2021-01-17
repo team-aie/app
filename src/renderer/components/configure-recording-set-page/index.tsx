189c71f6f19cad9d0647f997c5ea29bf2023ccac
garretje <garretje@rose-hulman.edu>
2021-01-16 21:24:24 -0500

    load only pages that exist

--- src/renderer/components/configure-recording-set-page/index.tsx
+++ src/renderer/components/configure-recording-set-page/index.tsx
@@ -3,7 +3,7 @@ import React, { FC, MouseEventHandler, useState } from 'react';
 import { useLocalStorage, usePrevious } from 'react-use';
 
 import { Consumer, RecordingSet } from '../../types';
-import { getLSKey } from '../../utils';
+import { checkFileExistence, getLSKey } from '../../utils';
 
 import { ConfigureRecordingSet } from './configure-recording-set';
 import './show-details.scss';
@@ -15,7 +15,8 @@ import { PreviewPage } from './preview-page';
  'external' represents pages external from this system
   - it's used to determine which transition should be used for the entering home page
 */
-export type RecordingPageState = 'home' | 'list-preview' | 'oto-ini' | 'dvcfg' | 'external';
+export type RecordingPageState = 'home' | 'external' | 'metadata';
+export type MetadataState = 'list-preview' | 'oto-ini' | 'dvcfg';
 
 /*
 Base page to handle configure-recording-set, dvcfg, oto.ini, and list-preview pages
@@ -26,9 +27,18 @@ const ConfigureRecordingSetPage: FC<{
   onSetSelected: Consumer<RecordingSet>;
 }> = ({ onNext, onBack, onSetSelected }) => {
   const [recordingSetState, setRecordingSetState] = useState<RecordingPageState>('external');
+  const [metadataStateIndex, setMetadataStateIndex] = useState<number>(-1);
   const prevState = usePrevious(recordingSetState) ?? 'home';
   const [transition, setTransition] = useState<boolean>(true);
+  const [filePathRec, setFilePathRec] = useState<string>('');
+  const [filePathOto, setFilePathOto] = useState<string>('');
+  const [filePathDvcfg, setFilePathDvcfg] = useState<string>('');
 
+  const [dropDownStates, setDropdownStateArray] = useState<MetadataState[]>([]);
+
+  const setDropdownState = () => {
+    setMetadataStateIndex((metadataStateIndex + 1) % dropDownStates.length);
+  };
   const [chosenBuiltInList = '', rawSetChosenBuiltInList] = useLocalStorage(
     getLSKey('ConfigureRecordingSetPage', 'chosenBuiltInList'),
     '',
@@ -38,29 +48,78 @@ const ConfigureRecordingSetPage: FC<{
     '',
   );
 
-  let filePathOto = 'external\\';
-  let filePathRec = 'external\\';
-  let filePathDvcfg = 'external\\';
+  const updatePaths = (listName: string, isBuiltIn: boolean) => {
+    const getFilePath = (metaDataState: MetadataState, listName: string, isBuiltIn: boolean) => {
+      let filePath = 'external\\';
 
-  if (chosenBuiltInList != '') {
-    switch (chosenBuiltInList) {
-      case 'デルタ式英語リストver5 (Delta English Ver. 5)':
-        filePathOto = filePathOto + 'delta_eng_ver5\\mkototemp.ini';
-        filePathRec = filePathRec + 'delta_eng_ver5\\デルタ式engver5_reclist.txt';
-        filePathDvcfg = filePathDvcfg + ''; //To be addded later
+      const stateToPathDeltaEnglish = {
+        'list-preview': 'delta_eng_ver5\\mkototemp.ini',
+        'oto-ini': 'delta_eng_ver5\\デルタ式engver5_reclist.txt',
+        dvcfg: 'To be added later',
+      };
 
-        break;
-      case 'Z式CVVC-Normal (Z Chinese CVVC - Normal)':
-        filePathOto = filePathOto + 'z.cvvc_normal\\oto.ini';
-        filePathRec = filePathRec + 'z.cvvc_normal\\Reclist.txt';
-        filePathDvcfg = filePathDvcfg + ''; //To be addded later
-        break;
-    }
-  } else {
-    filePathOto = filePathOto + chosenCustomListPath + '\\' + 'oto.ini';
-    filePathRec = filePathRec + chosenCustomListPath + '\\' + 'Reclist.txt';
-    filePathDvcfg = filePathDvcfg + chosenCustomListPath + '\\' + 'voice.dvcfg';
-  }
+      const stateToPathChinese = {
+        'list-preview': 'z.cvvc_normal\\oto.ini',
+        'oto-ini': 'z.cvvc_normal\\Reclist.txt',
+        dvcfg: 'To be added later',
+      };
+
+      const stateToCustListEnding = {
+        'list-preview': 'oto.ini',
+        'oto-ini': 'Reclist.txt',
+        dvcfg: 'voice.dvcfg',
+      };
+      if (isBuiltIn) {
+        switch (listName) {
+          case 'デルタ式英語リストver5 (Delta English Ver. 5)':
+            filePath = filePath + stateToPathDeltaEnglish[metaDataState];
+            break;
+          case 'Z式CVVC-Normal (Z Chinese CVVC - Normal)':
+            filePath = filePath + stateToPathChinese[metaDataState];
+            break;
+        }
+      } else {
+        filePath = listName + stateToCustListEnding[metaDataState];
+      }
+
+      return filePath;
+    };
+    const otoFilePath = getFilePath('oto-ini', listName, isBuiltIn);
+    const listFilePath = getFilePath('list-preview', listName, isBuiltIn);
+    const dvcfgFilePath = getFilePath('dvcfg', listName, isBuiltIn);
+
+    const otoPromise = checkFileExistence(otoFilePath);
+    const listPromise = checkFileExistence(listFilePath);
+    const dvcfgPromise = checkFileExistence(dvcfgFilePath);
+
+    Promise.all([otoPromise, listPromise, dvcfgPromise]).then((responses) => {
+      const states: MetadataState[] = [];
+      if (responses[0] == 'file') {
+        states.push('oto-ini');
+        setFilePathOto(otoFilePath);
+      } else {
+        setFilePathOto('');
+      }
+      if (responses[1] == 'file') {
+        states.push('list-preview');
+        setFilePathRec(listFilePath);
+      } else {
+        setFilePathRec('');
+      }
+      if (responses[2] == 'file') {
+        states.push('dvcfg');
+        setFilePathDvcfg(dvcfgFilePath);
+      } else {
+        setFilePathDvcfg('');
+      }
+      if (states.length == 0) {
+        setMetadataStateIndex(-1);
+      } else {
+        setDropdownStateArray(states);
+        setMetadataStateIndex(0);
+      }
+    });
+  };
 
   switch (recordingSetState) {
     case 'home':
@@ -76,6 +135,8 @@ const ConfigureRecordingSetPage: FC<{
           rawSetChosenBuiltInList={rawSetChosenBuiltInList}
           chosenCustomListPath={chosenCustomListPath}
           rawSetChosenCustomListPath={rawSetChosenCustomListPath}
+          metaDataIndex={metadataStateIndex}
+          getFilePath={updatePaths}
         />
       );
     case 'external':
@@ -91,44 +152,51 @@ const ConfigureRecordingSetPage: FC<{
           rawSetChosenBuiltInList={rawSetChosenBuiltInList}
           chosenCustomListPath={chosenCustomListPath}
           rawSetChosenCustomListPath={rawSetChosenCustomListPath}
+          metaDataIndex={metadataStateIndex}
+          getFilePath={updatePaths}
         />
       );
-    case 'list-preview':
-      return (
-        <PreviewPage
-          setRecordingSetState={setRecordingSetState}
-          leftPage="dvcfg"
-          rightPage="oto-ini"
-          pageName="List Preview"
-          transition={transition}
-          setTransition={setTransition}
-          fileName={filePathRec}
-        />
-      );
-    case 'oto-ini':
-      return (
-        <PreviewPage
-          setRecordingSetState={setRecordingSetState}
-          leftPage="list-preview"
-          rightPage="dvcfg"
-          pageName="Oto.ini"
-          transition={transition}
-          setTransition={setTransition}
-          fileName={filePathOto}
-        />
-      );
-    case 'dvcfg':
-      return (
-        <PreviewPage
-          setRecordingSetState={setRecordingSetState}
-          leftPage="oto-ini"
-          rightPage="list-preview"
-          pageName="Dvcfg"
-          transition={transition}
-          setTransition={setTransition}
-          fileName={filePathDvcfg}
-        />
-      );
+    case 'metadata':
+      if (dropDownStates[metadataStateIndex] == 'list-preview') {
+        return (
+          <PreviewPage
+            setRecordingSetState={setRecordingSetState}
+            pageName="List Preview"
+            transition={transition}
+            setTransition={setTransition}
+            fileName={filePathRec}
+            setDropDownState={setDropdownState}
+          />
+        );
+      } else if (dropDownStates[metadataStateIndex] == 'oto-ini') {
+        return (
+          <PreviewPage
+            setRecordingSetState={setRecordingSetState}
+            pageName="Oto.ini"
+            transition={transition}
+            setTransition={setTransition}
+            fileName={filePathOto}
+            setDropDownState={setDropdownState}
+          />
+        );
+      } else if (dropDownStates[metadataStateIndex] == 'dvcfg') {
+        return (
+          <PreviewPage
+            setRecordingSetState={setRecordingSetState}
+            pageName="Dvcfg"
+            transition={transition}
+            setTransition={setTransition}
+            fileName={filePathDvcfg}
+            setDropDownState={setDropdownState}
+          />
+        );
+      } else {
+        const error = new Error(
+          `Unknown metadata pageState: ${dropDownStates[metadataStateIndex]} with index: ${metadataStateIndex}`,
+        );
+        log.error(error);
+        throw error;
+      }
     default: {
       const error = new Error(`Unknown pageState: ${recordingSetState}`);
       log.error(error);
