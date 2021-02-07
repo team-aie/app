71afa6c3f5621e79b9d3de97086b2a1c59d7091b
Iluminar <yi.iluminar@gmail.com>
2021-02-07 13:03:00 -0800

    Apply code split using React.lazy

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
-import AieApp from './aie-app';
 import './services/i18n';
 import AppLoadFallback from './components/app-load-fallback';
 import { isDevelopment } from './env-and-consts';
@@ -16,9 +15,11 @@ if (!isDevelopment) {
   document.body.style.overflow = 'hidden';
 }
 
+const AieAppAsync = React.lazy(() => import('./aie-app'));
+
 ReactDOM.render(
   <Suspense fallback={<AppLoadFallback />}>
-    <AieApp />
+    <AieAppAsync />
   </Suspense>,
   document.getElementById('app'),
 );
