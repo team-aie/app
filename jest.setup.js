b466599e52bd2a6961eb236f8393b9deecd61a9c
Iluminar <yi.iluminar@gmail.com>
2021-02-23 22:44:24 -0800

    Use Node.js native TextDecoder

--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,7 @@
 
 {
   // This is a polyfill to match Node's implementation with Chromium's, we will not need them after we update to Node 14
-  const { TextEncoder, TextDecoder } = require('text-decoding');
+  const { TextEncoder, TextDecoder } = require('util');
 
   global.TextEncoder = TextEncoder;
   global.TextDecoder = TextDecoder;
@@ -12,7 +12,7 @@
   // `jest` is defined.
   // eslint-disable-next-line no-undef
   jest.mock('react-i18next', () => ({
-    // this mock makes sure any components using the translate hook can use it without a warning being shown
+    // This mock makes sure any components using the translate hook can use it without a warning being shown.
     useTranslation: () => {
       return {
         t: (str) => str,
