3be6d41f93248a4ab1a3c8ecd9af8bded62619c9
robinlingyunli <lil4@rose-hulman.edu>
2021-05-01 21:06:51 -0400

    fix feedback

--- src/renderer/utils/localstorage-clear.test.tsx
+++ src/renderer/utils/localstorage-clear.test.tsx
@@ -2,27 +2,30 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import { RETAINED_LOCALSTORAGE_KEYS, useResumeCheck } from './localstorage-clear';
 
-it('should call goToRecordingPage if confirm is true', () => {
-  window.confirm = jest.fn().mockImplementation(() => true);
-  for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
-    localStorage.setItem(RETAINED_LOCALSTORAGE_KEYS[i], 'value');
-  }
-  const goToRecordingPage = jest.fn();
-  renderHook(() => useResumeCheck(goToRecordingPage));
-  localStorage.clear();
+describe('project state resume test', () => {
+  it('if user clicks confirm', () => {
+    window.confirm = jest.fn().mockImplementation(() => true);
+    for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
+      localStorage.setItem(RETAINED_LOCALSTORAGE_KEYS[i], 'value');
+    }
+    const goToRecordingPage = jest.fn();
+    renderHook(() => useResumeCheck(goToRecordingPage));
+    localStorage.clear();
 
-  expect(goToRecordingPage).toHaveBeenCalledTimes(1);
-});
+    expect(goToRecordingPage).toHaveBeenCalledTimes(1);
+  });
 
-it('should delete stored values in localstorage if confirm is false', () => {
-  window.confirm = jest.fn().mockImplementation(() => false);
-  for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
-    localStorage.setItem(RETAINED_LOCALSTORAGE_KEYS[i], 'value');
-  }
+  it('if user clicks cancel', () => {
+    window.confirm = jest.fn().mockImplementation(() => false);
+    for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
+      localStorage.setItem(RETAINED_LOCALSTORAGE_KEYS[i], 'value');
+    }
 
-  const goToRecordingPage = jest.fn();
-  renderHook(() => useResumeCheck(goToRecordingPage));
-  for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
-    expect(localStorage.getItem(RETAINED_LOCALSTORAGE_KEYS[i])).toBeNull();
-  }
+    const goToRecordingPage = jest.fn();
+    renderHook(() => useResumeCheck(goToRecordingPage));
+    localStorage.clear();
+    for (let i = 0; i < RETAINED_LOCALSTORAGE_KEYS.length; i++) {
+      expect(localStorage.getItem(RETAINED_LOCALSTORAGE_KEYS[i])).toBeNull();
+    }
+  });
 });
